f2dd5258b157b2e640175da49434dd6221d7b593
Magdalena Król <leningrand@gmail.com>
2020-05-09 17:12:57 +0200

    Add project files.

---
+++ ClosetOrganizer/Models/ClothItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClosetOrganizer.Models
+{
+    public class ClothItem
+    {
+        public int ClothId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public ClothingStyle Style { get; set; }
+        public ClothingColor Color { get; set; }
+        public Category Category { get; set; }
+        public bool IsWearable { get; set; }
+
+    }
+
+
+    public enum ClothingStyle
+    {
+        None,
+        Casual,
+        BusinessCasual,
+        Formal,
+        Vintage
+    }
+
+    public enum ClothingColor
+    {
+        None,
+        Black,
+        White,
+        Blue,
+        Pink
+    }
+}
---
+++ ClosetOrganizer/Models/IClothItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClosetOrganizer.Models
+{
+    interface IClothItemRepository
+    {
+        IEnumerable<ClothItem> AllClothes { get; }
+        ClothItem GetClothById(int clothId);
+    }
+}
